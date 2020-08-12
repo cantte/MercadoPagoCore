32d3614c64fe5c7edd6f1afc04125342677e12e4
Carlos Pavajeau <cantte098@gmail.com>
2020-08-12 16:06:31 -0500

    Add Test and Examples projects

---
+++ MercadoPagoCore.Examples/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MercadoPagoCore.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
