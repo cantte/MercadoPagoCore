2b619f661a4001e39ab2662e02287d9ffe29dc09
Carlos Pavajeau <cantte098@gmail.com>
2020-08-14 18:53:26 -0500

    Add packages references

--- MercadoPagoCore.Examples/Program.cs
+++ MercadoPagoCore.Examples/Program.cs
@@ -1,4 +1,5 @@
-using System;
+using MercadoPagoCore.DataStructures.Payment;
+using MercadoPagoCore.Resources;
 
 namespace MercadoPagoCore.Examples
 {
@@ -6,7 +7,25 @@ namespace MercadoPagoCore.Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MercadoPagoSDK.AccessToken = "TEST-2823460721006149-081117-77b75c2b5a55f9b50c63417d40210f45-251462384";
+
+            Payment payment = new Payment
+            {
+                TransactionAmount = float.Parse("90000"),
+                Token = "534e4aab1346057efa768ad50609fba4",
+                Description = "Cats",
+                PaymentMethodId = "visa",
+                Installments = 1,
+                Payer = new Payer
+                {
+                    Email = "test@test.com"
+                },
+            };
+
+            if (payment.Save())
+            {
+                System.Console.WriteLine(payment.Status);
+            }
         }
     }
 }
