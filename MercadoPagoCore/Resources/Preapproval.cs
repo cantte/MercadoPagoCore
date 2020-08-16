ae07e0b595cb5f1a5b5e2c89482b04c8e0dea27f
Carlos Pavajeau <cantte098@gmail.com>
2020-08-16 10:31:42 -0500

    Resources/Preapproval: Add actions methods

--- MercadoPagoCore/Resources/Preapproval.cs
+++ MercadoPagoCore/Resources/Preapproval.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using MercadoPagoCore.Core;
 using MercadoPagoCore.DataStructures.Preapproval;
+using MercadoPagoCore.Net;
 
 namespace MercadoPagoCore.Resources
 {
@@ -17,5 +19,49 @@ namespace MercadoPagoCore.Resources
         public AutoRecurring? AutoRecurring { get; set; }
         [StringLength(256)]
         public string ExternalReference { get; set; }
+
+        public static Preapproval FindById(string id)
+        {
+            return FindById(id, WITHOUT_CACHE, null);
+        }
+
+        [GETEndpoint("/preapproval/:id")]
+        public static Preapproval FindById(string id, bool useCache, RequestOptions requestOptions)
+        {
+            return (Preapproval)ProcessMethod<Preapproval>(typeof(Preapproval), "FindById", id, useCache, requestOptions);
+        }
+
+        public bool Save()
+        {
+            return Save(null);
+        }
+
+        [POSTEndpoint("/preapproval")]
+        public bool Save(RequestOptions requestOptions)
+        {
+            return ProcessMethodBool<Preapproval>("Save", WITHOUT_CACHE, requestOptions);
+        }
+
+        public bool Update()
+        {
+            return Update(null);
+        }
+
+        [PUTEndpoint("/preapproval/:id")]
+        public bool Update(RequestOptions requestOptions)
+        {
+            return ProcessMethodBool<Preapproval>("Update", WITHOUT_CACHE, requestOptions);
+        }
+
+        public static List<Preapproval> Search(Dictionary<string, string> filters)
+        {
+            return Search(filters, WITHOUT_CACHE, null);
+        }
+
+        [GETEndpoint("/preapproval/search")]
+        public static List<Preapproval> Search(Dictionary<string, string> filters, bool useCache, RequestOptions requestOptions)
+        {
+            return ProcessMethodBulk<Preapproval>(typeof(Preapproval), "Search", filters, useCache, requestOptions);
+        }
     }
 }
